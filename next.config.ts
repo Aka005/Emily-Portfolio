2ebc00a26bb8dd82f18c262c910f6b5b6caa6ea0
Emily J <epj5179@psu.edu>
2026-07-10 09:50:02 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Emily-Portfolio",
-  assetPrefix: "/Emily-Portfolio/",
   images: {
     unoptimized: true,
   },
+  basePath: "/Emily-Portfolio",
 };
 
 export default nextConfig;
